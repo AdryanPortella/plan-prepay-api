b4da9a39e49850b3a07a461196a62b8639001a35
Seu Nome <adryanportella8@gmail.com>
2026-04-21 21:56:36 -0300

    chore: migration setup and create user table

---
+++ src/migrations/001_crete-users.sql
@@ -0,0 +1,12 @@
+
+-- create-users
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+CREATE TABLE IF NOT EXISTS users (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  name VARCHAR(255) NOT NULL,
+  cpf VARCHAR(14) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW()
+);
